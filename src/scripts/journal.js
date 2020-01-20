33476fc5cf43647c794a30724fc000fc758ac81f
Cooper Nichols <coopnich92@gmail.com>
2020-01-20 14:22:47 -0600

    commit DOM manipulation

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -5,18 +5,47 @@ let journalEntries = [
         title: "Group Presentation",
         content: "Blah blah blah",
         mood: "Spicy",
-    }
+    },
     {
         date: "1/16/2020",
         title: "DOM Stuff",
         content: "Blah blah bleh",
         mood: "Calm",
-    }
+    },
     {
         date: "1/17/2020",
-    title: "Scary object stuff",
-    content: "Blaaaaaaaaah",
-    mood: "Spoopy",
-    }
+        title: "Scary object stuff",
+        content: "Blaaaaaaaaah",
+        mood: "Tired",
+    },
 ]
 
+
+
+
+const createJournalEntry = (date, title, content, mood) => {
+    return `
+        <div>
+            <h2>${title}</h2>
+            <h3>${date}</h3>
+            <section>${content}</section>
+        </div> `
+}
+
+
+const journalContainer = document.querySelector(".entryLog")
+
+
+const renderJournalEntries = (entries) => {
+    for (let i = 0; i < entries.length; i++) {
+        const entry = entries[i]
+        journalContainer.innerHTML += createJournalEntry(
+            entry.title,
+            entry.date,
+            entry.content
+        )
+    }
+    return
+}
+
+renderJournalEntries(journalEntries);
