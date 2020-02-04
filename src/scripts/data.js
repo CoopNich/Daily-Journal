7998a5bfd821fd3a2efde94b54029f321d58827a
Cooper Nichols <coopnich92@gmail.com>
2020-02-04 15:22:22 -0600

    fixed Chained Promises

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -4,6 +4,15 @@ const API = {
        return fetch("http://localhost:3000/entries")
             .then(entry => entry.json())
             
+    },
+    saveJournalEntry (entryObject) {
+        return fetch("http://localhost:3000/entries", {
+      method: "POST",
+      headers: {
+         "Content-Type": "application/json"
+      },
+      body: JSON.stringify(entryObject)
+   })
     }
 }
 
--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,17 +1,39 @@
 import API from "./data.js"
 import renderEntries from "./entriesDOM.js"
+import addEntryToDom from "./entriesDOM.js"
 
 
 API.getJournalEntries()
-.then(parsedEntries => {
-   renderEntries(parsedEntries)
-})
+   .then(parsedEntries => {
+      renderEntries(parsedEntries)
+   })
 
 const journalSubmit = document.querySelector("#submit_button")
 
-journalSubmit.addEventListener ("click", event => {
+const journalMood = document.querySelector("#mood")
+const journalEntry = document.querySelector("#journalEntry")
+const journalDate = document.querySelector("#journalDate")
+const journalConcepts = document.querySelector("#concepts")
 
-
+const createNewEntryFactory = (date, title, content, mood) => {
+   return {
+      "date": date,
+      "title": title,
+      "content": content,
+      "mood": mood
+   }
 }
-)
+
+
+
+journalSubmit.addEventListener("click", event => {
+   const createNewEntry = createNewEntryFactory(journalDate.value, journalConcepts.value, journalEntry.value, journalMood.value)
+   API.saveJournalEntry(createNewEntry)
+      .then(() => {
+         API.getJournalEntries()
+         .then(renderEntries)
+      })
+   })
+
+
 
